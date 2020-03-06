93dbb596248daebe81ad2586f2ce58e38138fcf9
Dorsaf Ayed <dorsafa@users.noreply.github.com>
2020-03-06 11:47:10 +0100

    update

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func Add(a int, b int)int{
@@ -28,9 +30,26 @@ func ints(vs[]int)int{
 	}
 	return ints(vs[1:]) + vs[0]
 }
+func doubleHandler(w http.ResponseWriter, r *http.Request){
+	text := r.FormValue("v")
+	if text == "" {
+		http.Error(w,"missing value", http.StatusBadRequest)
+		return
+	}
+	// Atoi (string to int)
+	v,err := strconv.Atoi(text)
+	if err != nil {
+		http.Error(w,"not a number :" + text, http.StatusBadRequest)
+		return
+	}
+	fmt.Fprintln(w,v*2)
+
+}
+
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/",RootEndpoint).Methods("GET")
+	router.HandleFunc("/double",doubleHandler)
 	log.Fatal(http.ListenAndServe(":555",router))
 
 }
